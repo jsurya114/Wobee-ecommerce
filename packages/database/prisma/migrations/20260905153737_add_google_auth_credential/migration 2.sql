f9d605f160f73506919f023a5679ab2f6c543c4f
jsurya114 <jayasuryabrocamp@gmail.com>
2026-09-12 14:58:30 +0530

    chore: remove accidental duplicate ' 2' files
    31 stray files with ' 2' suffixes (macOS Finder copy artifacts) were
    accidentally committed. These are exact copies of their properly-named
    originals and serve no purpose.

--- packages/database/prisma/migrations/20260905153737_add_google_auth_credential/migration 2.sql
+++
@@ -1,8 +0,0 @@
--- AlterEnum
-ALTER TYPE "AuthMethod" ADD VALUE 'GOOGLE';
-
--- AlterTable
-ALTER TABLE "auth_credentials" ADD COLUMN     "providerSubject" TEXT;
-
--- CreateIndex
-CREATE UNIQUE INDEX "auth_credentials_providerSubject_key" ON "auth_credentials"("providerSubject");
--- packages/database/prisma/migrations/20260906044231_order_packed_rto_refund_unique/migration 2.sql
+++
@@ -1,6 +0,0 @@
--- AlterEnum
-ALTER TYPE "OrderStatus" ADD VALUE 'PACKED';
-ALTER TYPE "OrderStatus" ADD VALUE 'RETURNED_TO_ORIGIN';
-
--- CreateIndex
-CREATE UNIQUE INDEX "refunds_returnId_key" ON "refunds"("returnId");
